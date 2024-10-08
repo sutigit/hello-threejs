214094966f30ec72476734be36dc7a27ef642318
sutigit <panyanilben@gmail.com>
2024-10-08 22:18:00 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import './style.css'
+import * as THREE from 'three';
+
+const width = 1280;
+const height = 720;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+
+
+document.querySelector<HTMLDivElement>('#app')!.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
